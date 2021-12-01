738e798d0c943ca1d9f573fd1ad60c3753984730
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-01 23:23:01 +0100

    wip

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,45 +1,5 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { createModel } from 'xstate/lib/model'
-import { EventPayload, Oktopod } from '~/oktopod'
+import { Oktopod } from '~/oktopod'
 
-export function createBus() {
+export function factory(): Oktopod {
   return new Oktopod()
-}
-
-const model = createModel(
-  { event: null as EventPayload | null },
-  {
-    events: {
-      EVENT_A: (data: EventPayload) => data,
-      EVENT_B: (data: EventPayload) => data,
-      DONE: () => ({})
-    }
-  }
-)
-
-export function createTestMachine(id: string) {
-  return model.createMachine({
-    id,
-    strict: true,
-    preserveActionOrder: true,
-    initial: 'idle',
-    context: { event: null },
-    on: {
-      DONE: 'done'
-    },
-    states: {
-      idle: {
-        on: {
-          EVENT_A: {
-            actions: model.assign((_ctx, evt) => {
-              return {
-                event: { data: evt.data, event: evt.event }
-              }
-            })
-          }
-        }
-      },
-      done: {}
-    }
-  })
 }
--- src/oktopod.ts
+++ src/oktopod.ts
@@ -1,159 +1,73 @@
 import mitt from 'mitt'
 import invariant from 'tiny-invariant'
-import { Interpreter, InterpreterStatus } from 'xstate'
 
-export type EventPayload<TData = unknown> = {
+export type Payload<TData = unknown> = {
   event: string
   data: TData
 }
-
-function normalizeListener(cb: (data: EventPayload) => void): () => void {
-  return (...args: any[]): void => {
-    const data = args.length > 0 ? args[args.length - 1] : undefined
-    cb(data)
-  }
-}
-
 export class Oktopod {
   protected bus: ReturnType<typeof mitt>
 
-  protected machineToEvents: Map<
-    Interpreter<any, any, any>,
-    Map<string, () => void>
-  >
-
-  protected idToMachine: Map<string, Interpreter<any, any, any>>
-
-  protected listenerToWrapper: Map<
-    (...args: any[]) => void,
-    (...args: any[]) => void
-  >
+  //ovo nije dobro - moram da mapiram
+  // event + id = listener
+  protected idToListener: Map<string, any>
 
   constructor() {
     this.bus = mitt()
-    this.machineToEvents = new Map()
-    this.idToMachine = new Map()
-    this.listenerToWrapper = new Map()
+    this.idToListener = new Map()
   }
 
-  on(event: string, listener: (payload: EventPayload) => void): () => void
-
-  on(
-    event: string,
-    listener: Interpreter<any, any, any, any>,
-    send: string
-  ): () => void
-
-  on(
-    event: string,
-    listener:
-      | Interpreter<any, any, any, any>
-      | ((payload: EventPayload) => void),
-    send?: string
-  ): () => void {
-    if (listener instanceof Interpreter) {
-      //we have machine service
+  //TODO - koristiti handler type is mitt-a
+  on(event: string, listener: (...args: unknown[]) => void, id?: string): void {
+    if (id) {
       invariant(
-        send,
-        `When using machine as listener, please provide send type`
+        !this.idToListener.get(id),
+        `Listener with ${id} is already registered `
       )
-      const listenerData = this.machineToEvents.get(listener)
 
-      if (listenerData) {
-        const unregisterHandler = listenerData.get(event)
-        if (unregisterHandler) {
-          /* istanbul ignore next */
-          if (__DEV__) {
-            console.warn(
-              `Event: ${event} is already registered for service: ${listener.id}`
-            )
-          }
-
-          return unregisterHandler
-        }
-      } else {
-        this.machineToEvents.set(listener, new Map())
-        this.idToMachine.set(listener.id, listener)
-      }
-
-      const machineListener = normalizeListener((data: EventPayload) => {
-        if (listener.status !== InterpreterStatus.Running) {
-          /* istanbul ignore next */
-          if (__DEV__) {
-            console.warn(
-              `Event ${data.event} not sent to service: ${listener.id} because the service is not running`
-            )
-          }
-
-          return
-        }
-
-        if (listener.state.nextEvents.findIndex((evt) => evt === send) < 0) {
-          /* istanbul ignore next */
-          if (__DEV__) {
-            console.warn(
-              `Event ${data.event} not sent to service ${listener.id} because the service does not accept event: ${send}`
-            )
-          }
-
-          return
-        }
-
-        listener.send(send, data)
-      })
-
-      const unregister = (): void => {
-        this.bus.off(event, machineListener)
-      }
-
-      this.bus.on(event, machineListener)
-      /* eslint-disable @typescript-eslint/no-non-null-assertion */
-      this.machineToEvents.get(listener)!.set(event, unregister)
-
-      return unregister
+      this.idToListener.set(id, listener)
     }
 
-    const wrappedListener = normalizeListener(listener)
-
-    this.listenerToWrapper.set(listener, wrappedListener)
-    this.bus.on(event, wrappedListener)
-
-    return () => {
-      this.bus.off(event, wrappedListener)
-      this.listenerToWrapper.delete(listener)
-    }
+    this.bus.on(event, listener)
   }
 
-  off(
-    event: string,
-    listener: Interpreter<any, any, any> | ((...args: any[]) => void)
-  ): void {
-    if (listener instanceof Interpreter) {
-      const listenerData = this.machineToEvents.get(listener)
-      if (listenerData) {
-        const unregisterHandler = listenerData.get(event)
-        unregisterHandler && unregisterHandler()
-        listenerData.delete(event)
-      }
-
-      return
-    }
-
-    const listenerWrapper = this.listenerToWrapper.get(listener)
-    if (listenerWrapper) {
-      this.bus.off(event, listenerWrapper)
-    }
-  }
-
-  clear(event: string): void {
-    this.machineToEvents.forEach((data) => {
-      data.delete(event)
-    })
-
-    this.bus.off(event)
-  }
-
-  emit(event: string, data?: unknown): void {
+  emit(event: string, data: unknown): void {
     this.bus.emit(event, { event, data })
+  }
+
+  get<T = unknown>(id: string): T {
+    return this.idToListener.get(id)
+  }
+
+  //TODO - ovo
+  off(event: string, listener: (...args: any[]) => void): void
+
+  off(event: string, id: string): void
+
+  off(event: string, id: string | ((...args: any[]) => void)): void {
+    // event = event ?? '*'
+    if (typeof id === 'string') {
+      //find the string and unregister
+      const listener = this.idToListener.get(id)
+      if (listener) {
+        this.bus.off(event, listener)
+      }
+    } else {
+      this.bus.off(event, id)
+    }
+
+    //TODO - this.bus.all , sta ima u keys  a sta u values
+    // off - event , listener
+    // off -  event, id
+    // off - all events , listener
+    // off - all events , id
+  }
+
+  clear(): void {
+    this.bus.all.clear()
+  }
+
+  destroy(): void {
+    this.clear()
   }
 }
