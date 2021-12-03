2e1ebb0c9d4b71d60da41be4866e236f95c9bf22
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-04 00:58:22 +0100

    implement simple function listener

---
+++ src/__tests__/function-listener.test.ts
@@ -0,0 +1,149 @@
+import { factory } from './utils'
+
+describe('Function listener', () => {
+  test('Register listener for a specific event', () => {
+    const bus = factory().bus()
+    const listener = jest.fn()
+    const event = 'event'
+
+    bus.on(event, listener)
+    bus.emit(event)
+
+    expect(listener).toHaveBeenCalledTimes(1)
+  })
+
+  test('Listener recieves event data', () => {
+    const bus = factory().bus()
+    const listener = jest.fn()
+    const event = 'event'
+    const data = { foo: 'bar' }
+
+    bus.on(event, listener)
+    bus.emit(event, data)
+
+    expect(listener).toHaveBeenCalledWith({ event, data })
+  })
+
+  test('Emit event with no data', () => {
+    const bus = factory().bus()
+    const listener = jest.fn()
+    const event = 'event'
+
+    bus.on(event, listener)
+    bus.emit(event)
+
+    expect(listener).toHaveBeenCalledWith({ event, data: undefined })
+  })
+
+  test('Register multiple listeners for a specific event', () => {
+    const bus = factory().bus()
+    const firstListener = jest.fn()
+    const secondListener = jest.fn()
+    const event = 'event'
+    const data = { foo: 'bar' }
+
+    bus.on(event, firstListener)
+    bus.on(event, secondListener)
+    bus.emit(event, data)
+
+    expect(firstListener).toHaveBeenCalledTimes(1)
+    expect(firstListener).toHaveBeenCalledWith({ event, data })
+    expect(secondListener).toHaveBeenCalledTimes(1)
+    expect(secondListener).toHaveBeenCalledWith({ event, data })
+  })
+
+  test('Regsiter listener for all events', () => {
+    const bus = factory().bus()
+    const listener = jest.fn()
+    const event = 'event'
+    const secondEvent = 'eventTwo'
+    const data = { foo: 'bar' }
+    const secondData = { foo: 'bar' }
+
+    bus.on('*', listener)
+    bus.emit(event, data)
+    bus.emit(secondEvent, secondData)
+
+    expect(listener).toHaveBeenCalledTimes(2)
+    expect(listener).toHaveBeenCalledWith({ event, data })
+    expect(listener).toHaveBeenCalledWith({
+      event: secondEvent,
+      data: secondData
+    })
+  })
+  describe('Unregister', () => {
+    test('Unregister listener for a specific event', () => {
+      const bus = factory().bus()
+      const listener = jest.fn()
+      const event = 'event'
+      const data = { foo: 'bar' }
+
+      bus.on(event, listener)
+      bus.off(event, listener)
+      bus.emit(event, data)
+
+      expect(listener).not.toHaveBeenCalled()
+    })
+
+    test('Unregister listener for all events', () => {
+      const bus = factory().bus()
+      const listener = jest.fn()
+      const event = 'event'
+      const data = { foo: 'bar' }
+      const secondEvent = 'eventTwo'
+      const secondData = { foo: 'bar' }
+
+      bus.on('*', listener)
+      bus.off('*', listener)
+      bus.emit(event, data)
+      bus.emit(secondEvent, secondData)
+
+      expect(listener).not.toHaveBeenCalled()
+    })
+
+    test('Unregister listener for specific event via returned unsubscribe function', () => {
+      const bus = factory().bus()
+      const listener = jest.fn()
+      const event = 'event'
+      const data = { foo: 'bar' }
+
+      const unsubscribe = bus.on(event, listener)
+      unsubscribe()
+      bus.emit(event, data)
+
+      expect(listener).not.toHaveBeenCalled()
+    })
+
+    test('Unregister listener for all events via returned unsubscribe function', () => {
+      const bus = factory().bus()
+      const listener = jest.fn()
+      const event = 'event'
+      const data = { foo: 'bar' }
+      const secondEvent = 'eventTwo'
+      const secondData = { foo: 'bar' }
+
+      const unsubscribe = bus.on('*', listener)
+      unsubscribe()
+      bus.emit(event, data)
+      bus.emit(secondEvent, secondData)
+
+      expect(listener).not.toHaveBeenCalled()
+    })
+
+    test('Clear all listeners for a specific event', () => {
+      const bus = factory().bus()
+      const firstListener = jest.fn()
+      const secondListener = jest.fn()
+      const event = 'event'
+      const data = { foo: 'bar' }
+
+      bus.on(event, firstListener)
+      bus.on(event, secondListener)
+      bus.clear(event)
+      bus.emit(event, data)
+
+      expect(firstListener).not.toHaveBeenCalled()
+      expect(secondListener).not.toHaveBeenCalled()
+    })
+  })
+})
